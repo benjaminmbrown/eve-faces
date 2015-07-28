9fa403352eefc7c2a071f7d4d5e0c7d5715f6834
Benjamin M. Brown <benjaminbb@gmail.com>
2015-07-28 10:07:11 -0500

    adding navbar store

--- app/actions/NavbarActions.js
+++ app/actions/NavbarActions.js
@@ -4,16 +4,17 @@ import {assign} from 'underscore';
 class NavbarActions {
 constructor(){
 	this.generateActions(
-		'updateOnlineUsers',
-		'updateAjaxAnimation',
-		'updateSearchQuery',
-		'getCharacterCountSuccess',
-		'getCharacterCountFail',
+		'updateOnlineUsers',//Sets online users count on Socket.IO event update.
+		'updateAjaxAnimation',//Adds "fadeIn" or "fadeOut" CSS class to the loading indicator.
+		'updateSearchQuery',//Update search query value on keypress.
+		'getCharacterCountSuccess',//Returns total number of characters.
+		'getCharacterCountFail',//Returns jQuery jqXhr object.
 		'findCharacterSuccess',
 		'findCharacterFail'
 		)
 }
 
+//Find a character by name.
 findCharacter(payload){
 	$.ajax({
 		url: '/api/characters/search',
@@ -27,7 +28,7 @@ findCharacter(payload){
 		this.actions.findCharacterFail(payload);
 	})
 }
-
+//Fetch total number of characters from the server.
 getCharacterCount(){
 	$.ajax({url:'/api/characters/count'})
 		.done((data)=>{
